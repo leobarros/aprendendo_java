b5cb482c2fab251d6bdee69f0bfc75cfa24fe0f5
Leonardo Barros <nickolback@gmail.com>
2014-12-31 07:27:27 -0200

    Criação da classe GradeBook
    Criando a classe e usando os métodos set e get

---
+++ GradeBook/GradeBook.java
@@ -0,0 +1,23 @@
+//Figura 3.7: GradeBook.java
+//classe GradeBook que contém uma variável de instância
+//courseName e métodos para configurar e obter seu valor
+
+public class GradeBook
+{
+  private String courseName; // nome do curso para esse GradeBook
+
+  public void setCourseName(String name) 
+  {
+    couseName = name;
+  }
+
+  public String getCourseName()
+  {
+    return courseName;
+  }
+
+  public void displayMessage()
+  {
+    System.out.printf("Welcome to the GradeBook for\n%s!\n" getCourseName());
+  }
+}
