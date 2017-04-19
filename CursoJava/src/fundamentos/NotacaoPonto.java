1e66cb7ef5bdc028c6fd488977c43b7d5f602f04
Leonardo Barros <nickolback@gmail.com>
2017-04-19 18:39:50 -0300

    Notação Ponto

---
+++ CursoJava/src/fundamentos/NotacaoPonto.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class NotacaoPonto {
+	
+	public static void main(String[] args) {
+		String s = "Bom dia ?".toUpperCase().replace("?", "Linda").concat("!");
+		System.out.println(s);
+	}
+
+}
