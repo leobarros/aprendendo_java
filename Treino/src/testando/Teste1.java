212be5ef45721ba7604e5a2045594307bad94228
Leonardo Barros <nickolback@gmail.com>
2017-04-19 19:04:07 -0300

    Criando área de teste

---
+++ Treino/src/testando/Teste1.java
@@ -0,0 +1,14 @@
+package testando;
+
+public class Teste1 {
+	
+	public static void main(String[] args) {
+		String nome = "Leonardo Barros";
+		String turma = "Linguagem de Programação em Java";
+		
+		System.out.println(nome + " fez o curso de " + turma);
+		
+		
+	}
+
+}
