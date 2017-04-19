f2086545f70d855b78bc3a8eee558c21e6edc79d
Leonardo Barros <nickolback@gmail.com>
2017-04-19 19:03:39 -0300

    Usando imports

---
+++ CursoJava/src/fundamentos/Import.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+import java.awt.Button;
+import java.util.Date;
+
+public class Import {
+	
+	public static void main(String[] args) {
+		System.out.println("testando imports ...");
+		
+		
+		// Exemplo ilustrativo do uso de imports.
+		// O código abaixo não faz nada.
+		
+		Button b;
+		Date d;
+		
+		// Usei a tecla de atalho Ctrl + Shift + O
+		// para organizar os imports 
+	}
+
+}
