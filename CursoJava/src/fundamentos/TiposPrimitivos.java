b3f1b7b010d3c03a311d14bbe737a0844241c4bb
Leonardo Barros <nickolback@gmail.com>
2017-04-22 05:56:56 -0300

    Tipos Primitivos

---
+++ CursoJava/src/fundamentos/TiposPrimitivos.java
@@ -0,0 +1,21 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+	
+	public static void main(String[] args) {
+		//tipo primitivos booleano
+		boolean bo1 = false;
+		boolean bo2 = true;
+		
+		System.out.printf("%b %b \n", bo1, bo2);
+		
+		// tipo primitivo caracter
+		char c1 = 'a';
+		char c2 = '!';
+		char c3 = '\u0050';
+		
+		System.out.printf("%c, %c, %c \n", c1, c2,c3);
+		
+	}
+
+}
