6c5d4178f959e97c3b44b2bd9d0afcc4a0bc1d78
Leonardo Sales <lsales@globosat.com.br>
2017-04-22 13:17:17 -0300

    Adicionando mais tipos primitivos

--- CursoJava/src/fundamentos/TiposPrimitivos.java
+++ CursoJava/src/fundamentos/TiposPrimitivos.java
@@ -16,6 +16,24 @@ public class TiposPrimitivos {
 		
 		System.out.printf("%c, %c, %c \n", c1, c2,c3);
 		
+		// Tipos primitivos inteiros
+		byte b = 127;
+		short s = 32767;
+		int i = 2_147_483_647;
+		long l = 9_223_372_036_854_775_807L;
+		
+		System.out.printf("%d %d %d %d", b, s, i, l);
+		System.out.println(Integer.toBinaryString(b));
+		System.out.println(Integer.toBinaryString(s));
+		System.out.println(Integer.toBinaryString(i));
+		System.out.println(Long.toBinaryString(l));
+		
+		// Tipos primitivos reais (ponto flutuante)
+		float f = 3.1416F;
+		double d = 2.45;
+		
+		System.out.printf("%.2f %.1f", f, d);
+		
 	}
 
 }
