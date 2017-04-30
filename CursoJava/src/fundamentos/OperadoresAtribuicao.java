fd134352d5e45e2f4d72cc142f484b2b29141eac
Leonardo Sales <lsales@globosat.com.br>
2017-04-30 17:02:03 -0300

    Operadores Atribuicao

---
+++ CursoJava/src/fundamentos/OperadoresAtribuicao.java
@@ -0,0 +1,19 @@
+package fundamentos;
+
+public class OperadoresAtribuicao {
+	
+	public static void main(String[] args) {
+		
+		int a = 7;
+		int b = 3;
+		
+		b += a;
+		b *= 4;
+		b /= 2;
+		b -= 2;
+		//b %= 2;
+		
+		System.out.println(b);
+	}
+
+}
