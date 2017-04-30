b2de285ebc5e75625fda3c569a8299925c00f02d
Leonardo Barros <nickolback@gmail.com>
2017-04-29 23:20:38 -0300

    Teste 2

---
+++ Treino/src/testando/Teste2.java
@@ -0,0 +1,28 @@
+package testando;
+
+public class Teste2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Testando...");
+		
+		String nome = "Leonardo Barros".toUpperCase();
+		String estudando = "Fundamentos de Java".toUpperCase();
+		Double nota = 10.000000;
+		final double PI = 3.1416;
+		
+		double raio = 3.5;
+		double area = PI * raio * raio;
+		
+		System.out.println(nome + " está estudando " + estudando);
+		System.out.printf("Nota do curso: %.2f\n", nota);
+		
+		// matematica
+		System.out.println("Valor da área: " + area);
+		
+		
+		
+		
+	}
+
+}
