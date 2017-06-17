48ce1525c2d456311ccc2e801d08fac59d7a50e1
Leonardo Sales <lsales@globosat.com.br>
2017-06-17 15:57:06 -0300

    Testando o laco for

---
+++ Treino/src/testando/Teste3.java
@@ -0,0 +1,21 @@
+package testando;
+
+public class Teste3 {
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			System.out.print(i + " ");
+
+		}
+		System.out.println();
+		
+		for (int j = 1; j <= 10; j++) {
+			System.out.print(j + " ");
+		}
+		System.out.println();
+		
+		for (int h = 10; h >=1; h --) {
+			System.out.print(h + " ");
+		}
+	}
+}
