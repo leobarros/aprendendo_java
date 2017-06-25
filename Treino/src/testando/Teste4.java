9ca4fa0a0c67db8f65da35467ddaa993915f4688
Leonardo Sales <lsales@globosat.com.br>
2017-06-25 11:38:52 -0300

    Testando o while

---
+++ Treino/src/testando/Teste4.java
@@ -0,0 +1,23 @@
+package testando;
+
+public class Teste4 {
+	public static void main(String[] args) {
+		
+		int numero = 0;
+		
+		while(numero <= 10) {
+			System.out.print(numero + " ");
+			numero ++;
+		}
+		
+		System.out.println();
+		
+		int conta = 10;
+		
+		while (conta >= 0) {
+			System.out.print(conta + " ");
+			conta --;
+		}
+		
+	}
+}
